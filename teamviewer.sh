c5de6a76f3a8c35995520b5eb758912242ff1174
Jonathan Morrison <jonathan.morrison@gmail.com>
2018-05-08 10:52:16 -0600

    use the correct teamviewer version

--- teamviewer.sh
+++ teamviewer.sh
@@ -4,6 +4,6 @@
 
 sudo apt-get -y install gdebi-core wget
 cd ~/Downloads
-wget https://download.teamviewer.com/download/teamviewer_i386.deb
+wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
 sudo gdebi -n teamviewer*.deb
 rm -f teamviewer*.deb
