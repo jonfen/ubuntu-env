5d4e53afcded13f42bf0f00bec1c904886cda92d
Jonathan Morrison <jonathan.morrison@gmail.com>
2018-09-01 16:13:15 -0600

    add offline freecodecamp

---
+++ freecodecamp.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Installs
+./javascript.sh
+./mongodb.sh
+
+sudo apt install git
+
+mkdir -p ~/dev/github.com/FreeCodeCamp
+cd ~/dev/github.com/FreeCodeCamp
+
+git clone https://github.com/FreeCodeCamp/freecodecamp
+cd freecodecamp
+npm install
+cp sample.env .env
+npm run only-once
+npm run develop
+
+open http://localhost:3000
