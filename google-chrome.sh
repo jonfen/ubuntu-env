6d9341ff8b375fb7326a350ad9cc161cd2f8e678
Jonathan Morrison <jonathan.morrison@gmail.com>
2019-03-06 16:51:47 -0700

    add note for non-supported video hardware

--- google-chrome.sh
+++ google-chrome.sh
@@ -8,3 +8,6 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
 sudo apt update
 sudo apt install google-chrome-stable
+
+# On some installations chrome is really slow, Disable Hardware Acceleration to fix
+# chrome://settings/?search=hardware
