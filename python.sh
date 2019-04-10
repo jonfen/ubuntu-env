348a7336c3a943116365dcfdad3caedcbf9cda9c
Jonathan Morrison <jonathan.morrison@gmail.com>
2019-04-10 14:15:20 -0600

    add python 3.7

--- python.sh
+++ python.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # Install Python3
-sudo apt install -y python3-pip
+sudo apt install -y python3-pip python3-dev
 sudo apt install build-essential libssl-dev libffi-dev python-dev
+
+# Upgrade to Python 3.7
+sudo apt install python3.7 python3.7-dbg python3.7-doc python3.7-minimal python3.7-dev python3.7-examples python3.7-venv
 
 # Install IDLE (Integrated Development and Learning Environment)
 sudo apt install -y idle
