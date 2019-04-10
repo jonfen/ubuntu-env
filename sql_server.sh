34912b3f926a609d74d57e7ba7b90e7e44bab914
Jonathan Morrison <jonathan.morrison@gmail.com>
2019-04-10 14:16:36 -0600

    add odbc drivers

--- sql_server.sh
+++ sql_server.sh
@@ -3,10 +3,19 @@
 # https://docs.microsoft.com/en-us/sql/linux/sql-server-linux-setup-tools?view=sql-server-2017
 # https://docs.microsoft.com/en-us/sql/connect/jdbc/using-kerberos-integrated-authentication-to-connect-to-sql-server?view=sql-server-2017
 # https://docs.microsoft.com/en-us/sql/azure-data-studio/download?view=sql-server-2017
+# https://docs.microsoft.com/en-us/sql/connect/odbc/linux-mac/installing-the-microsoft-odbc-driver-for-sql-server?view=sql-server-2017
 
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/18.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
 sudo apt-get update 
+sudo ACCEPT_EULA=Y apt-get install msodbcsql17
+# optional: for bcp and sqlcmd
+sudo ACCEPT_EULA=Y apt-get install mssql-tools
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
+source ~/.bashrc
+# optional: for unixODBC development headers
+sudo apt-get install unixodbc-dev
 sudo apt-get -y install mssql-tools unixodbc-dev libunwind8
 
 # cd ~/Downloads
