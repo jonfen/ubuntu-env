e18c64e25c45a19dd03944b0e453eb3786e5da29
Jonathan Morrison <jonathan.morrison@gmail.com>
2019-04-17 10:18:08 -0600

    Update version

--- desktop/flowcharts.sh
+++ desktop/flowcharts.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# https://www.yworks.com/downloads#yEd
+# Install Dia AND yEd
 
 sudo apt -y install wget dia libcanberra-gtk-module libcanberra-gtk3-module
 
-INSTALL_SCRIPT="yEd-3.18.2_with-JRE10_64-bit_setup.sh"
+# https://www.yworks.com/downloads#yEd
+INSTALL_SCRIPT="yEd-3.19_with-JRE11_64-bit_setup.sh"
 cd ~/Downloads
 wget https://www.yworks.com/resources/yed/demo/$INSTALL_SCRIPT
 chmod a+x $INSTALL_SCRIPT 
