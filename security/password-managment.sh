0d482b95eeb326b25d315283958c16f8e518f0be
Jonathan Morrison <jonathan.morrison@imlss.com>
2019-05-22 16:18:08 -0600

    add keepass2

--- security/password-managment.sh
+++ security/password-managment.sh
@@ -5,6 +5,11 @@
 
 sudo apt -y install pass
 
+# KeePass2
+
+sudo add-apt-repository ppa:jtaylor/keepass
+sudo apt -y install keepass2
+
 ## LastPass & CLI setup
 # https://lastpass.com/lplinux.php
 
