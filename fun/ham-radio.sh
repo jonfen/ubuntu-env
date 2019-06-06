4d9d57c2c3d1ede03f5244a78342c2606c20511c
Jonathan Morrison <jonathan.morrison@gmail.com>
2019-06-05 21:50:49 -0600

    add Elecraft KX3 installation

--- fun/ham-radio.sh
+++ fun/ham-radio.sh
@@ -6,3 +6,15 @@
 sudo apt-add-repository ppa:dansmith/chirp-snapshots
 sudo apt update
 sudo apt -y install chirp-daily
+
+# Elecraft KX3
+# https://elecraft.com/pages/firmware-software
+
+sudo dpkg --add-architecture i386
+sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386 libgtk2.0-0:i386 
+mkdir -p ~/Downloads/kx3
+cd ~/Downloads/kx3
+wget http://www.elecraft.com/software/KX3/KX3UtilityLINUX_1_16_6_25.tgz
+tar -zxf KX3UtilityLINUX_1_16_6_25.tgz
+cd kx3util_1_16_6_25 
+./kx3util
