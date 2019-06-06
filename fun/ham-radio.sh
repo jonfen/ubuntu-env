7182249b9d03dcbf3fea740d1c16e2606e2c8cc6
Jonathan Morrison <jonathan.morrison@gmail.com>
2019-06-05 21:51:46 -0600

    add wget to radio install

--- fun/ham-radio.sh
+++ fun/ham-radio.sh
@@ -11,7 +11,7 @@ sudo apt -y install chirp-daily
 # https://elecraft.com/pages/firmware-software
 
 sudo dpkg --add-architecture i386
-sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386 libgtk2.0-0:i386 
+sudo apt-get install wget libc6:i386 libncurses5:i386 libstdc++6:i386 libgtk2.0-0:i386 
 mkdir -p ~/Downloads/kx3
 cd ~/Downloads/kx3
 wget http://www.elecraft.com/software/KX3/KX3UtilityLINUX_1_16_6_25.tgz
