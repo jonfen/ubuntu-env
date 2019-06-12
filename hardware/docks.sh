35c00739b6142c23181a7251adbe0b59c5b248b1
Jonathan Morrison <jonathan.morrison@imlss.com>
2019-06-12 14:30:08 -0600

    add docking station notes

---
+++ hardware/docks.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# USB-C™ Universal QUAD HD (QVHD) Docking Station : DOCK520
+# https://support.displaylink.com/knowledgebase/articles/684649
+# https://us.targus.com/pages/driver-support
+# https://support.displaylink.com/knowledgebase/articles/1181623-displaylink-ubuntu-driver-after-recent-x-upgrades
+
+
