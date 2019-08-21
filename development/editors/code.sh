4d2f231c73f40a651cea2a20a11799db25ea205e
Jonathan Morrison <jonathan.morrison@imlss.com>
2019-08-21 09:00:39 -0600

    make code-insiders the default git editor

--- development/editors/code.sh
+++ development/editors/code.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-sudo apt -y install snapd
+sudo apt -y install snapd git
 
 snap install code --classic
 snap install code-insiders --classic
+
+# Make default editor for git
+git config --global core.editor "code-insiders --wait"
+git config --global difftool.vscode.cmd 'code-insiders --wait --diff $LOCAL $REMOTE'
