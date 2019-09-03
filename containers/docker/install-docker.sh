5e48c7894feebbb3aee517ce9e783c5d3893811d
Jonathan Morrison <jonathan.morrison@gmail.com>
2019-09-03 10:37:50 -0600

    update recommended docker install instructions

--- containers/docker/install-docker.sh
+++ containers/docker/install-docker.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# https://docs.docker.com/install/linux/docker-ce/ubuntu/
+
 # kill it all
 sudo apt-get purge -y docker-engine docker docker.io docker-ce docker-ce-cli
-sudo apt-get autoremove -y --purge docker-engine docker docker.io docker-ce docker-ce-cli 
+sudo apt-get autoremove -y --purge docker-engine docker docker.io docker-ce docker-ce-cli containerd runc
 sudo rm -rf /var/lib/docker
 sudo rm /etc/apparmor.d/docker
 sudo groupdel docker
@@ -11,13 +13,12 @@ sudo rm -rf /var/run/docker.sock
 # Install Docker-CE
 echo "Use the Docker repo, not the Ubuntu version"
 sudo apt update
-apt-cache policy docker-ce
-sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
+sudo apt -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 echo "9DC8 5822 9FC7 DD38 854A E2D8 8D81 803C 0EBF CD88"
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable"
-sudo apt -y install docker-ce
+sudo apt -y install docker-ce docker-ce-cli containerd.io
 
 sudo systemctl enable docker
 # sudo systemctl status docker
