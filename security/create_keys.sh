9ec164e1b6a84b94fe01a1ae2b1f2a11df369520
Jonathan Morrison <jonathan.morrison@gmail.com>
2021-09-25 09:22:10 -0600

    fix permission error

--- security/create_keys.sh
+++ security/create_keys.sh
@@ -8,6 +8,11 @@
 ssh-keygen -t ed25519 -C $(hostname)-ed25519 -P "" -f /home/$(whoami)/.ssh/id_ed25519
 # ssh-keygen -o -t rsa -b 4096 -C $(hostname)-rsa -P "" -f /home/$(whoami)/.ssh/id_rsa
 
+# Check permissions
+chmod 700 ~/.ssh/
+chmod 600 ~/.ssh/*
+chmod 400 ~/.ssh/id_*
+
 # Copy to clipboard
 if [ -z `which xclip` ]; then sudo apt -y install xclip; fi
 xclip -sel clip < ~/.ssh/id_ed25519.pub
@@ -20,9 +25,4 @@ ssh-add ~/.ssh/id_ed25519
 # ssh-add ~/.ssh/id_rsa
 ssh-add -l
 echo "You can use 'ssh-copy-id' to send your public keys to a linux server"
-
-# Check permissions
-chmod 700 ~/.ssh/
-chmod 600 ~/.ssh/*
-chmod 400 ~/.ssh/id_*
 
